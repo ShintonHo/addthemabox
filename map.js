cba90829f446bcb3086bca3a1876c1ad1dc6e187
ShintonHo <HEXITONG1@GMAIL.COM>
2019-11-21 12:12:45 -0500

    Update map.js

--- map.js
+++ map.js
@@ -2,8 +2,8 @@ mapboxgl.accessToken = 'pk.eyJ1Ijoia2h5c24iLCJhIjoiY2pnbGJubDdoMDBmbjJ3bzFzdGg1a
 var map = new mapboxgl.Map({
 container: 'map',
 style: 'mapbox://styles/khysn/ck38222z00v2m1cpjrghx7ktx',
-center: [-85.38255, 38.73836],
-zoom: 15.38,
-pitch: 60.00,
-bearing: -9.60
+center: [-85.37960, 38.72637],
+zoom: 12.85,
+pitch: 0.00,
+bearing: 0.00
 });
