c89feaeb876f0ce995b1618901424fe6da289ae7
shintonho <hexitong1@gmail.com>
2019-11-21 01:23:49 -0500

    update

---
+++ map.js
@@ -0,0 +1,9 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2h5c24iLCJhIjoiY2pnbGJubDdoMDBmbjJ3bzFzdGg1amhrOSJ9.invAhppOjjNxmU5IeF4kwg';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/khysn/ck38222z00v2m1cpjrghx7ktx',
+center: [-85.38255, 38.73836],
+zoom: 15.38,
+pitch: 60.00,
+bearing: -9.60
+});
